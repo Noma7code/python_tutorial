6c4b0a2047fd5609a415cbbe25caade29d426297
Noma7code <168642014+Noma7code@users.noreply.github.com>
2024-07-04 11:43:18 +0000

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,51 @@
+print(4+5) #addition
+print(4*5) #multiplication
+print(6-8.2) # subtraction
+print(83*3)# substraction
+
+savings=100
+# Create the variables monthly_savings and num_months
+monthly_savings=10
+num_months=4
+# Multiply monthly_savings and num_months
+new_savings=monthly_savings*num_months
+
+# Add new_savings to your savings
+total_savings =new_savings+savings
+
+
+# Print total_savings
+print(total_savings)
+
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings=monthly_savings*num_months
+
+# Print the type of year_savings
+print(type(year_savings))
+
+doubleintro=intro+intro   # Assign sum of intro and intro to doubleintro
+
+
+
+# Print out doubleintro
+print(doubleintro)
+
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout. changing vaariable int to str
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+
+
+# Convert pi_string into float: pi_float
+pi_float=float(pi_string)
+print("he is hungry" + str(savings)+"Henry")
